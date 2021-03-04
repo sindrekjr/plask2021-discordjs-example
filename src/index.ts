4766c015065306c63629c565551c3f83b15d2ff2
Sindre S. Kjær <sindre.sperrud.kjaer@spv.no>
2021-03-04 09:48:02 +0100

    chore: updated packages, resolve compile target issue, and add cmd_prefix to env

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import dotenv from 'dotenv';
 import { CommandoClient } from 'discord.js-commando';
+
 import { SvadaCommand } from './commands';
 
 dotenv.config();
 
-const { BOT_OWNER, BOT_TOKEN } = process.env;
+const { BOT_OWNER, BOT_TOKEN, CMD_PREFIX } = process.env;
 
 const client = new CommandoClient({
-  commandPrefix: '/',
+  commandPrefix: CMD_PREFIX,
   owner: BOT_OWNER,
 });
 
 client.registry
   .registerDefaults()
-  .registerCommand(SvadaCommand);
+  .registerGroup('examples', 'Examples')
+  .registerCommands([SvadaCommand]);
 
 client.login(BOT_TOKEN);
